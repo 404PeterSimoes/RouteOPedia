d1f74a2d075db4092a506bf5d67950d95998c0df
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-04-22 15:33:24 +0100

    Section 8 - 139 - Nested Routes in Action

--- src/layout/ProductLayout.jsx
+++ src/layout/ProductLayout.jsx
@@ -1,3 +1,5 @@
+import { NavLink, Outlet } from 'react-router-dom';
+
 function ProductLayout() {
   return (
     <div className="container-fluid">
@@ -5,11 +7,35 @@ function ProductLayout() {
         <div className="col-12">
           <h1 className="mb-3">Products</h1>
           <p>Browse our complete product catalog</p>
+
           <nav className="mb-3 border rounded">
             <div className="p3">
-              <div className="d-flex gap-2 flex-wrap">ALL PRODUCTS</div>
+              <div className="d-flex gap-2 flex-wrap">
+                <NavLink to="/products" end className="btn btn-outline-success">
+                  All Products
+                </NavLink>
+                <NavLink
+                  to="/products/electronics"
+                  className="btn btn-outline-success"
+                >
+                  Electronics
+                </NavLink>
+                <NavLink
+                  to="/products/clothing"
+                  className="btn btn-outline-success"
+                >
+                  Clothing
+                </NavLink>
+                <NavLink
+                  to="/products/books"
+                  className="btn btn-outline-success"
+                >
+                  Books
+                </NavLink>
+              </div>
             </div>
           </nav>
+          <Outlet />
         </div>
       </div>
     </div>
